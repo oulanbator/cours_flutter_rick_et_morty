7c7f3423730a50e6d63d708e95276c49193ef141
Victor Matheron <victor.matheron@gmail.com>
2024-03-07 14:50:36 +0100

    Adds a comment of a non standard navigation transition example

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -30,9 +30,34 @@ Widget _listElement(context, character) {
     subtitle: Text(character.species),
     onTap: () => Navigator.push(
       context,
+
+      // MaterialPageRoute implemente une transition standard pour la Librairie Material (Android)
+
       MaterialPageRoute(
         builder: (context) => DetailsPage(character: character),
       ),
+
+      // On peut utiliser PageRouteBuilder au lieu de MaterialPageRoute pour customiser la navigation
+      // PageRouteBuilder est construit autour de la 'page' et de la 'transition'.
+      // Sans rentrer dans le détail, voici un exemple issu de la documentation.
+      // Décommentez 'PageRouteBuilder' ci-dessous et commentez 'MaterialPageRoute' plus haut pour voir
+      // un exemple de transition pas très standard ^^...
+
+      // PageRouteBuilder<void>(
+      //   opaque: false,
+      //   pageBuilder: (BuildContext context, _, __) =>
+      //       DetailsPage(character: character),
+      //   transitionsBuilder:
+      //       (___, Animation<double> animation, ____, Widget child) {
+      //     return FadeTransition(
+      //       opacity: animation,
+      //       child: RotationTransition(
+      //         turns: Tween<double>(begin: 0.5, end: 2.0).animate(animation),
+      //         child: child,
+      //       ),
+      //     );
+      //   },
+      // ),
     ),
   );
 }
